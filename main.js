c8c26026df56dbd3b0a5db6d1e24fbe8b0769d27
Rafael Hartung <rafaelhartung@outlook.com>
2024-05-15 08:09:46 +0200

    Updated Files so format is 00:00:00 in the clock

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 function DisplayTime() {
-    // Define a New Date Object
-    var now = new Date()
-    // Same for Hours Minutes and so on
-    var hour = now.getHours() // 0-23
-    var minutes = now.getMinutes() // 0-59
-    var seconds = now.getSeconds() // 0-59
-    var daytime = "Morgens"
-    // Define Variable for what need
-    var time = hour + ":" + minutes + ":" + seconds + ' ' + daytime
-    document.getElementById("clockdisplay").innerHTML = time
-    document.getElementById("clockdisplay").textContent = time
-    setTimeout (DisplayTime, 1000)
+    var now = new Date();
+    var hour = String(now.getHours()).padStart(2, '0'); // Add leading zero if necessary
+    var minutes = String(now.getMinutes()).padStart(2, '0'); // Add leading zero if necessary
+    var seconds = String(now.getSeconds()).padStart(2, '0'); // Add leading zero if necessary
+    var daytime = "Uhr";
+    var time = hour + ":" + minutes + ":" + seconds + ' ' + daytime;
+    document.getElementById("clockdisplay").innerHTML = time;
+    document.getElementById("clockdisplay").textContent = time;
+    setTimeout(DisplayTime, 1000);
 }
 
 DisplayTime();
